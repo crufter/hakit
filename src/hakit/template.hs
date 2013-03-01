e7b5eae0aed33d0571da7b1f2072654dcfb7367c
Dobronszki János <janosdobronszki@citromail.hu>
2013-03-01 08:55:23 +0100

    In the middle of it.

--- src/hakit/template.hs
+++ src/hakit/template.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
+module Template where
+
 import qualified Data.Text as T
 import qualified Data.List as L
 import qualified Text.ParserCombinators.Parsec as P
+import qualified Data.List.Split as Spl
 
 -- This package contains some questionable temporary names now to avoid clash with prelude.
 
@@ -13,38 +16,48 @@ instance Show Attr where
 type Child = Tag
 
 data Tag =
-        Doctype 
-    |   HTML    [Attr] [Child]
-    |   Head    [Attr] [Child]
-    |   Body    [Attr] [Child]
-    |   Div     [Attr] [Child]
-    |   Text    T.Text
-    |   A       [Attr] [Child]
+        Doctype T.Text
+    |   Text    T.Text  
+    --          Name        Attributes  Children
+    |   Tag     T.Text      [Attr]      [Child]
 
-html a c    = HTML a c
-head' a c   = Head a c
-body a c    = Body a c
+getAttrs :: Tag ->      [Attr]
+getAttrs (Doctype t)    = []
+getAttrs (Text t)       = []
+getAttrs (Tag _ a _)    = a
+
+getAttr :: Tag -> T.Text -> Maybe T.Text
+getAttr tag attrName = case L.find (\(Attr (a, b)) -> a == attrName) (getAttrs tag) of
+    Just (Attr (a, b))  -> Just b
+    Nothing             -> Nothing
+
+getChildren :: Tag ->       [Child]
+getChildren (Doctype t)     = []
+getChildren (Text t)        = []
+getChildren (Tag _ _ c)     = c
+
+getName :: Tag ->      T.Text
+getName (Doctype t)    = "doctype"
+getName (Text t)       = "text"
+getName (Tag n _ _)    = n
+
+doctype a   = Doctype a
+html a c    = Tag "html" a c
+head' a c   = Tag "head" a c
+body a c    = Tag "body" a c
 text t      = Text t
-div' a c    = Div a c
+div' a c    = Tag "div" a c
+tag n a c   = Tag n a c
 
 attr :: T.Text -> T.Text -> Attr
 attr a b = Attr (a, b)
 
 example :: Tag
 example =
-    HTML [] [
-        Head [] [],
-        Body [attr "style" "background: #ccc;"] [
-            Text "yo"
-        ]
-    ]
-
-example2 :: Tag
-example2 =
     html [] [
         head' [] [],
         body [attr "style" "background: #ccc;"] [
-            text "yo"
+            text "Hello world."
         ]
     ]
 
@@ -60,29 +73,113 @@ sc :: [Child] -> String
 sc x = L.intercalate "\n" $ map show x
 
 instance Show Tag where
-    show (HTML a b) = "<html" ++ sa a ++ ">" ++ sc b ++ "</html>"
-    show (Head a b) = "<head" ++ sa a ++ ">" ++ sc b ++ "</head>"
-    show (Body a b) = "<body" ++ sa a ++ ">" ++ sc b ++ "</body>"
-    show (Text a)   = T.unpack a
+    show (Doctype a)    = "<!DOCTYPE " ++ T.unpack a ++ ">"
+    show (Tag n a b)    = "<" ++ T.unpack n ++ sa a ++ ">" ++ sc b ++ "</" ++ T.unpack n ++ ">"
+    show (Text a)       = T.unpack a
 
--- Planned supported selectors.
--- *                    - everything
--- #X                   - id selector
--- .X                   - class selector
--- X Y                  - descendant selector
--- X                    - type selector
--- X > Y                - direct child selector
--- X[attrName]          - has attribute selector
--- X[attrName="val"]    - attribute name-value selector
--- X[attrName*="val"]   - regexp attribute selectors
--- X[attrName^="val"]
--- X[attrName$="val"]
--- :not(selector)       - negation pseudoclass selector
--- :nth-child(3)
--- :nth-last-child(3)
+-- | Just for quick and ugly testing.
+-- Tests if a (top level) tag satisfies a given selector
+-- (which can contain multiple criteria, like "div.className").
+-- Obviously selectors like "div a" or "div > a" won't work in this case.
+matches :: T.Text -> Tag -> Bool
+matches sel tag = all (flip satisfiesSingle $ tag) (parseSelector sel)
 
--- map, just named differently, to avoid clash with Prelude.
--- change
+parseSelector :: T.Text -> [Selector]
+parseSelector t = let sels = P.parse parseExpr "selector" $ T.unpack t in
+    case sels of
+        Left e      -> error $ show e
+        Right ss    -> ss
+
+satisfiesSingle :: Selector -> Tag -> Bool
+satisfiesSingle s tag = case s of
+    Type t  -> getName tag == t
+    Id  id  -> case getAttr tag "id" of
+        Just x  -> x == id
+        Nothing -> False
+    Class c -> case getAttr tag "class" of
+        Just x  -> x == c
+        Nothing  -> False
+    Attribute reg attrName attrVal -> case getAttr tag attrName of
+        Nothing -> False
+        Just x  -> case reg of
+            StartsWith  -> T.isPrefixOf attrVal x
+            EndsWith    -> T.isSuffixOf attrVal x
+            Contains    -> T.isInfixOf attrVal x
+            Equals      -> x == attrVal
+            Any         -> True
+    Descendant      -> error $ "can't use descendant separator on: " ++ show tag
+    DirectChild     -> error $ "can't use direct child separator on: " ++ show tag
+
+satisfies :: [Tag] -> Tag -> [[Selector]] -> Bool
+satisfies parents tag sels =
+    let separator x = case x of
+            Descendant  -> True
+            DirectChild -> True
+            otherwise   -> False
+        nonseps = L.filter separator sels
+        -- ps = Parents satisfy
+        -- Called with a sels list ending in a separator, and
+        -- with sels' having even length. Eg: [selector, sep, selector, sep]
+        ps parents sels'
+            | sels == []        = True
+            | parents == []     = False     -- And sels /= []
+            | otherwise         =
+                let sep = last sels'
+                    crit = last . last $ sels'
+                in case last sels' of
+                    Descendant      -> if satisfiesSingle $ last parents
+                        then ps (init . init $ sels') $ init parents
+                        else ps sels' $ init parents
+                    DirectChild     -> if satisfiesSingle $ last parents
+                        then ps (init . init $ sels') $ init parents
+                        else False
+    -- Obviously if there are fewer parents than parent criterias, or the given tag does not
+    -- satisfy the given criteria, we can stop.
+    in if not $ satisfiesSingle (last nonseps) tag || length parents < length nonseps - 1
+        then False
+        else if length sels == 1
+            then True
+            -- We only get here if sels has an odd length, length sels > 1 
+            else ps parents $ init sels
+
+-- Apply function on elements matching the selector.
+alter :: T.Text -> Tag -> (Tag -> Tag) -> Tag
+alter sel t f =
+    let sels = parseSelector t
+        selsGrouped = (Spl.split . Spl.oneOf) [Descendant, DirectChild] sels
+        alterRec :: Tag -> [Tag] -> Tag
+        alterRec tag parents = case tag of
+            Doctype t       -> appif tag
+            Text t          -> appif tag
+            Tag n a c       -> appif $ Tag n a $ map (\x -> alterRec (parents ++ [tag]) x) c
+            where
+                appif t =
+                    if satisfies parents tag selsGrouped
+                        then f t
+                        else t
+    in alterRec t []
+
+{--------------------------------------------------------------------
+  Selectors.  
+--------------------------------------------------------------------}
+
+-- Selectors planned
+
+-- Implemented      Example                     Description
+--                  *                           - everything
+-- Y                #X                          - id selector
+-- Y                .X                          - class selector
+--                  X Y                         - descendant selector
+-- Y                X                           - type selector
+--                  X > Y                       - direct child selector
+-- Y                [attrName]                  - has attribute selector
+-- Y                [attrName="val"]            - attribute name-value selector
+-- Y                [attrName*="val"]           - regexp attribute selectors
+-- Y                [attrName^="val"]  
+-- Y                [attrName$="val"]  
+--                  selector:not(selector)      - negation pseudoclass selector
+--                  selector:nth-child(3)   
+--                  selector:nth-last-child(3)  
 
 data Regexy =
         StartsWith
@@ -96,14 +193,14 @@ data Selector =
         Type        T.Text
     |   Id          T.Text
     |   Class       T.Text
-    |   Attribute   Regexy T.Text T.Text T.Text     -- Regex type, tag name, attrname, attrval
-    -- These are more like relations between selectors and not selectors themselves.s
+    |   Attribute   Regexy T.Text T.Text     -- Regex type, tag name, attrname, attrval
+    -- These are more like relations between selectors and not selectors themselves, but hey.
     |   Descendant
     |   DirectChild
     deriving (Show)
 
 {--------------------------------------------------------------------
-  Parsers.  
+  Parsering selectors.  
 --------------------------------------------------------------------}
 
 parseString :: P.Parser T.Text
@@ -124,7 +221,7 @@ parseNonquoted = do
 
 parseDescendant :: P.Parser Selector
 parseDescendant = do
-    P.many P.space
+    P.space
     return Descendant
 
 parseDirectChild :: P.Parser Selector
@@ -153,31 +250,29 @@ parseTyp = do
 
 parseAttr :: P.Parser Selector
 parseAttr = do
-    typ <- parseNonquoted
-    P.many P.space
     P.char '['
     attrName <- parseNonquoted
     mode <- P.many $ P.string "*=" P.<|> P.string "^=" P.<|> P.string "$=" P.<|> P.string "="
     val <- P.many $ parseNonquoted P.<|> parseString
     P.char ']'
     return $ case mode of
-        []          -> Attribute Any        attrName    typ     ""
-        ["*="]      -> Attribute Contains   attrName    typ     (val!!0)
-        ["^="]      -> Attribute StartsWith attrName    typ     (val!!0)
-        ["$="]      -> Attribute EndsWith   attrName    typ     (val!!0)
-        ["="]       -> Attribute Equals     attrName    typ     (f val)
+        []          -> Attribute Any        attrName    ""
+        ["*="]      -> Attribute Contains   attrName    (val!!0)
+        ["^="]      -> Attribute StartsWith attrName    (val!!0)
+        ["$="]      -> Attribute EndsWith   attrName    (val!!0)
+        ["="]       -> Attribute Equals     attrName    (f val)
     where
         f :: [T.Text] -> T.Text
         f x = if length x == 0
             then ""
             else x!!0
 
-parseExpr :: P.Parser Selector
-parseExpr = P.try parseId
+parseExpr :: P.Parser [Selector]
+parseExpr = P.many1 $ P.try parseId
     P.<|> P.try parseClass
     P.<|> P.try parseAttr
     P.<|> P.try parseTyp
     P.<|> P.try parseDirectChild
     P.<|> P.try parseDescendant
 
--- parse parseExpr "selector" input
+-- > P.parse parseExpr "selector" "#id"
